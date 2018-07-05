91571ac6f4af275e842efcee46e8e57bad510806
karim <kwach@hotmail.fr>
2018-07-05 02:40:29 +0200

    creation habitat categorierepository not exist...

--- resources/views/habitat.blade.php
+++ resources/views/habitat.blade.php
@@ -6,20 +6,20 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
+            <h4 class="modal-title" id="gridSystemModalLabel">Ajouter une activité</h4>
           </div>
           <div class="modal-body">
+
             <div class="row">
               <div class="col-md-4">.col-md-4</div>
               <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
             </div>
+
             <div class="row">
               <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
               <div class="col-md-2 col-md-offset-4">.col-md-2 .col-md-offset-4</div>
             </div>
-            <div class="row">
-              <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
-            </div>
+
             <div class="row">
               <div class="col-sm-9">
                 Level 1: .col-sm-9
@@ -33,10 +33,11 @@
                 </div>
               </div>
             </div>
+
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button id="btnPasserActivite" type="button" class="btn btn-default" data-dismiss="modal">Passer</button>
+            <button id="btnAjoutActivite" type="button" class="btn btn-primary">Ajouter</button>
           </div>
         </div>
       </div>
