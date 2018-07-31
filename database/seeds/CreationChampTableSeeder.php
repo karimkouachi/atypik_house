5f9721c7f34ecf6a5423d7e92d16f0118171a3cb
Marvin PINTO <marvin.pinto@Hydra.local>
2018-07-31 09:52:11 +0200

    merge karim

--- database/seeds/CreationChampTableSeeder.php
+++ database/seeds/CreationChampTableSeeder.php
@@ -14,10 +14,7 @@ class CreationChampTableSeeder extends Seeder
     {
     	DB::table('creation_champ')->insert([
     		[
-<<<<<<< HEAD
-=======
                 'label_champ' => 'Ville',
->>>>>>> 214e01043d1256bb1313c837a95252ad02d97bf9
     			'libelle_champ' => 'ville_habitat',
     			'longueur_champ' => 255,
                 'null_champ' => 0,
